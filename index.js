1b48e4bb0609f1793bd3d5ac48391492a5d10147
lgy <happygy@wwway.biz>
2022-06-15 14:35:59 +0900

    4

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000
 
 const path = require('path')
 
+app.use('/api/data', (req, res) => res.json({data: 'data', port: PORT}))
+
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'client/build')))
 // 라우트 설정
@@ -15,7 +17,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'))
 })
 
-app.use('/api/data', (req, res) => res.json({data: 'data', port: PORT}))
 
 app.listen(PORT, () => {
   console.log(`server running port : ${PORT}!!`)

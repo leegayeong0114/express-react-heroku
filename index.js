197716bc53423601a228b3b31a1917bf8381c267
lgy <happygy@wwway.biz>
2022-06-15 14:13:05 +0900

    2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express') 
-const app = express() // express 객체
+const app = express() // express 객체 생성
 
 const dotenv = require('dotenv')
 dotenv.config()
@@ -8,16 +8,14 @@ const PORT = process.env.PORT || 5000
 
 const path = require('path')
 
-// 리액트 정적 파일 제공
+// 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'client/build')))
 // 라우트 설정
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'))
 })
 
-app.get('/', (req, res) => {res.send('Server Response Success!')})
-
-app.get('/api/data', (req, res) => res.json({data: 'data', port: PORT}))
+app.use('/api/data', (req, res) => res.json({data: 'data', port: PORT}))
 
 app.listen(PORT, () => {
   console.log(`server running port : ${PORT}!!`)

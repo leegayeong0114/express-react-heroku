96329a6977e86c72699edb9aeef0b98614014b06
lgy <happygy@wwway.biz>
2022-06-15 11:09:40 +0900

    herokuku

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express') 
+const app = express() // express 객체
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/api/data', (req, res) => res.json({data: 'data'}))
+
+app.listen(PORT, () => {
+  console.log(`server running port : ${PORT}!!`)
+})
